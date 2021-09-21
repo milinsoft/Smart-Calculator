afdac11cba2606f3eee37b4b97fa2d2f1348234b
parseltongist <>
2021-09-21 18:30:53 +0200

    Stage 4/7 with eval function

--- calculator.py
+++ calculator.py
@@ -1,39 +1,46 @@
 import re
 
 
-def transform_expression(lst) -> list:
-    args = []
-    for arg in lst:
-        if arg.strip("-").isdigit():
-            args.append(int(arg))
-        elif re.match(r"-{2}", arg):
-            args.append("+")
-        elif re.match(r"-{3}", arg):
-            args.append("-")
-        elif re.match(r"\+{2,}", arg):
-            args.append("+")
-        else:
-            args.append(arg)
-    return args
+def transform_expression(string) -> str:
+    """Removing spaces"""
+    string = "".join(string.split())
+
+    if not re.match(r"[\w+\-]+$", string):
+        """ add * and devisors"""
+        """ add check that operators are not 1st and lst"""
+        print("Invalid expression")
+    elif len(string) == 1:
+        return string
+    else:
+        args = []
+        for arg in string:
+            if re.match(r"[-]", arg):
+                args.append("+" if len(arg) % 2 == 0 else "-")
+            elif re.match(r"\+{2,}", arg):
+                args.append("+")
+            else:
+                args.append(arg)
+        return "".join(args)
+
+
+def compute_expression(math_expression):
+    math_expression = [element for element in math_expression]
+    print(math_expression)
+
 
 
 if __name__ == '__main__':
     while True:
         arguments_str = input()
-        try:
-            arguments_list = [int(x) for x in arguments_str.split()]
-            print(sum(arguments_list)) if len(arguments_str) > 0 else ""
-        except ValueError:
-            if arguments_str == "/exit":
-                print("Bye!")
-                exit()
-            elif arguments_str == "/help":
-                print("The program calculates the sum of numbers"
-                      "bla bla bla")
-            else:
-                print("Argumets must be integers!")
+        if arguments_str == "/exit":
+            print("Bye!")
+            exit()
+        elif arguments_str == "/help":
+            print("The program calculates a given expression")
+        elif len(arguments_str) == 0:
+            continue
+        else:
+            expression = transform_expression(arguments_str)
+            print(eval(expression))  # this is a great build-in method to calculate the expression, but I need to try implement mine
+            #compute_expression(expression)
 
-
-# try to remove all "+" from the expression and
-# if there is a munis before armument - make it negative
-# sum all
