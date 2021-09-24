5b386226695d623c8581286f7396b4db062b8c84
parseltongist <>
2021-09-24 15:44:23 +0200

    Stage 6/7 draft test #4 error

--- calculator.py
+++ calculator.py
@@ -48,8 +48,6 @@ def prioritized_computation(math_expression):
             arg1 = int(math_expression.pop(math_expression[priority_index - 1]))  # minus one as we need the arg 1 before priority operator
             sign = math_expression.pop(math_expression[priority_index - 1])  # as arg1 already popped, now sign changed it's index
             arg2 = int(math_expression.pop(math_expression[priority_index - 1]))  # as sign already popped, now arg2 changed it's index
-            # dict realization doesn't work
-
             if sign == "/":
                 operation_result = arg1 / arg2
             elif sign == "*":
@@ -88,35 +86,41 @@ def compute_expression(math_expression):
 def var_handler(arguments_str, variables):
     expr_template = re.compile(r"(\A\w+(\s*[-\+*/]\s*\w+)+$)")
     """Print variable value scenario"""
-    if re.match(r"\a[a-z]+$", arguments_str, flags=re.IGNORECASE):
+    if re.match(r"\A\s*[a-z]+$", arguments_str, flags=re.IGNORECASE):
         if arguments_str in variables.keys():
             print(variables[arguments_str])
         else:
             print("Unknown variable")
 
-    elif re.search("=", arguments_str):
+        """ASSIGNMENT SCENARIO"""
+    elif re.search(r"=", arguments_str):
         var_assignment(arguments_str, variables)
 
+        """ expression with variables """
     elif re.match(expr_template, arguments_str):
         expression_list = var_operations(arguments_str)
-        expression_list = deque(expression_list)
-        return expression_list
+        exp_str = " ".join(expression_list)
+        transformed_expr =  transform_into_expression(exp_str)
+        return print_result(transformed_expr)
 
 
 def var_assignment(arguments_str, variables):
-    var_ass_template = re.compile(r"\A[a-z]+\s*=\s*\d+$", flags=re.IGNORECASE)
+    var_ass_template = re.compile(r"\A\s*[a-z]+\s*=\s*\w+\s*$", flags=re.IGNORECASE)
     if re.match(var_ass_template, arguments_str):
         """ scenario for var = int/float only """
-        var, val = re.split(r"\s*=\s*", arguments_str)
+        var, val = re.split(r"s*=\s*", arguments_str.replace(" ", ""))
+        var = var.strip(" ")
+        val = val.strip(" ")
         if val.isnumeric():
             variables[var] = val
         else:
             """reassignment"""
             if val in variables.keys():
-                variables[var] = val
+                variables[var] = variables[val]
             else:
                 print("Unknown variable")
-
+    elif re.match(r"\w+", arguments_str):
+        print("Invalid identifier")
     else:
         print("Invalid assignment")
 
@@ -125,18 +129,29 @@ def var_assignment(arguments_str, variables):
 def var_operations(arguments_str) -> list:
     var_list = re.findall(r"[a-z]+", arguments_str, flags=re.IGNORECASE)
     issue_counter = 0
-    for v in var_list:
-        if v not in variables.keys():
-            issue_counter += 1
-        if issue_counter != 0:
-            print("Unknown variable")
-        else:
-            arguments_lst = arguments_str.split(" ")
-            for vl in var_list:
-                for i in range(len(arguments_str)):
-                    if arguments_lst[i] == v and vl in variables.keys():
-                        arguments_lst[i] = variables[vl]
-            return arguments_lst
+    arguments_lst = arguments_str.split(" ")
+
+    for i in range(int(len(arguments_lst))):
+        if arguments_lst[i] in var_list:
+            if arguments_lst[i] in variables.keys():
+                arguments_lst[i] = variables[arguments_lst[i]]
+            else:
+                issue_counter += 1
+    if issue_counter != 0:
+        print("Unknown variable")
+    else:
+        return arguments_lst
+
+
+def print_result(expression_list):
+    if not expression_list:
+        pass
+    elif len(expression_list) == 1:
+        print(expression_list[0].lstrip("+"))
+    else:
+        expression_list = deque(expression_list)
+        result = compute_expression(expression_list)
+        print(result)
 
 
 if __name__ == '__main__':
@@ -150,28 +165,11 @@ if __name__ == '__main__':
             handle_command(arguments_str)
         elif arguments_str[-1] in {"+", "-", "/", "*"}:
             print("Invalid expression")
-        elif arguments_str[0].isalpha():
-            expression_list = var_handler(arguments_str, variables)
-            """ if there is an assignment here, expression_list will have no value
-            need to fix removing assigning stucture here"""
-
-            if len(expression_list) == 1:
-                print(expression_list[0].lstrip("+"))
-            else:
-                expression_list = deque(expression_list)
-                result = compute_expression(expression_list)
-                print(result)
+        elif arguments_str.lstrip()[0].isalpha():
+            var_handler(arguments_str, variables)
         else:
             expression_list = transform_into_expression(arguments_str)
-            #print(eval(expression))  # this is a great build-in method to calculate the expression, but I need to try implement mine
-            if len(expression_list) == 1:
-                print(expression_list[0].lstrip("+"))
-            else:
-                expression_list = deque(expression_list)
-                result = compute_expression(expression_list)
-                print(result)
-
-
+            print_result(expression_list)
 
 
 # test with input like just 1 positive number
