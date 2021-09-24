21d1cfdd367f14dd9f4a74b459a6b00f1f28d415
parseltongist <>
2021-09-24 20:15:59 +0200

    Stage 6/7 working code. dirty as f.

--- calculator.py
+++ calculator.py
@@ -8,6 +8,8 @@ def transform_into_expression(string) -> list:
     """Splitting arguments and signs into the list"""
     str_as_lst = string.split(" ")
     if re.search(r"(([-\+]?)\d+( ?[-\+\*/]+ ?\d+)+)", string):
+        """looks like this regex doesnt work with mixed expression"""
+
         for arg in range(len(str_as_lst)):
             if str_as_lst[arg] != " ":
                 if re.match(r"[\-]+$", str_as_lst[arg]):
@@ -64,7 +66,7 @@ def prioritized_computation(math_expression):
 # potentially merge top and not top calculations later on
 
 def compute_expression(math_expression):
-    math_expression = prioritized_computation(math_expression)
+    # math_expression = prioritized_computation(math_expression)  # turned of for the stage #6 as no prioritized operations for now
     operators = {"-", "+"}
     if len(math_expression) < 3:
         return "".join(math_expression)
@@ -84,14 +86,13 @@ def compute_expression(math_expression):
 
 
 def var_handler(arguments_str, variables):
-    expr_template = re.compile(r"(\A\w+(\s*[-\+*/]\s*\w+)+$)")
+    expr_template = re.compile(r"\A(\s*\w+\s*[-+*/]*\s*)+$")
     """Print variable value scenario"""
     if re.match(r"\A\s*[a-z]+$", arguments_str, flags=re.IGNORECASE):
         if arguments_str in variables.keys():
             print(variables[arguments_str])
         else:
             print("Unknown variable")
-
         """ASSIGNMENT SCENARIO"""
     elif re.search(r"=", arguments_str):
         var_assignment(arguments_str, variables)
@@ -99,47 +100,64 @@ def var_handler(arguments_str, variables):
         """ expression with variables """
     elif re.match(expr_template, arguments_str):
         expression_list = var_operations(arguments_str)
-        exp_str = " ".join(expression_list)
-        transformed_expr =  transform_into_expression(exp_str)
-        return print_result(transformed_expr)
+        if expression_list != 1:
+            exp_str = " ".join(expression_list)
+            transformed_expr = transform_into_expression(exp_str)
+            return transformed_expr
+        else:
+            return 1
 
 
 def var_assignment(arguments_str, variables):
-    var_ass_template = re.compile(r"\A\s*[a-z]+\s*=\s*\w+\s*$", flags=re.IGNORECASE)
+    var_ass_template = re.compile(r"\A\s*[a-z]+\s*=\s*([a-z]+|[\d]+)\s*$", flags=re.IGNORECASE)
     if re.match(var_ass_template, arguments_str):
+
         """ scenario for var = int/float only """
-        var, val = re.split(r"s*=\s*", arguments_str.replace(" ", ""))
+        var, val = re.split(r"\s*=\s*", arguments_str.replace(" ", ""))
         var = var.strip(" ")
         val = val.strip(" ")
         if val.isnumeric():
             variables[var] = val
+            return 0
         else:
             """reassignment"""
             if val in variables.keys():
                 variables[var] = variables[val]
+                return 0
             else:
                 print("Unknown variable")
+
+
     elif re.match(r"\w+", arguments_str):
         print("Invalid identifier")
+        return 1
     else:
         print("Invalid assignment")
+        return 1
 
 
 
 def var_operations(arguments_str) -> list:
-    var_list = re.findall(r"[a-z]+", arguments_str, flags=re.IGNORECASE)
+    var_list = re.findall(r"[\w]+", arguments_str, flags=re.IGNORECASE)
     issue_counter = 0
-    arguments_lst = arguments_str.split(" ")
+    arguments_lst = arguments_str.strip(" ").split(" ")
 
+    """ test just this function with the complex variable + int expression"""
     for i in range(int(len(arguments_lst))):
         if arguments_lst[i] in var_list:
             if arguments_lst[i] in variables.keys():
-                arguments_lst[i] = variables[arguments_lst[i]]
+                arguments_lst[i] = str(variables[arguments_lst[i]])
             else:
-                issue_counter += 1
+                if arguments_lst[i].isdigit():
+                    arguments_lst[i] = str(arguments_lst[i])
+                else:
+                    issue_counter += 1
     if issue_counter != 0:
         print("Unknown variable")
+        return 1
     else:
+        arguments_lst = transform_into_expression(" ".join(arguments_lst))
+        #print(arguments_lst)
         return arguments_lst
 
 
@@ -165,8 +183,14 @@ if __name__ == '__main__':
             handle_command(arguments_str)
         elif arguments_str[-1] in {"+", "-", "/", "*"}:
             print("Invalid expression")
+
+
         elif arguments_str.lstrip()[0].isalpha():
-            var_handler(arguments_str, variables)
+            expr = var_handler(arguments_str, variables)
+            if expr != 1:
+                print_result(expr)
+            else:
+                continue
         else:
             expression_list = transform_into_expression(arguments_str)
             print_result(expression_list)
