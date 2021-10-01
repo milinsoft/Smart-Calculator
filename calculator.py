62f909edc7e20f4fbc83d3d7fb077022fd82d481
parseltongist <>
2021-10-01 13:32:17 +0200

    Stage 6/7 working code. dirty code v1.5 updated some logic

--- calculator.py
+++ calculator.py
@@ -14,7 +14,7 @@ def handle_command(command):
         print("Unknown command")
 
 
-def transform_into_expression(string, variables) -> list:
+def transform_into_expression(string, variables) -> str:
     """this function takes input string and empty variables dict as arguments, analysis if provided expression is valid and
      formats an expression by replacing variables with its values and reducing sequence of repittive math operators.
      otherwise an error message is printer"""
@@ -25,24 +25,25 @@ def transform_into_expression(string, variables) -> list:
         print("Invalid expression")
 
     elif re.search(r"[a-z]+", string, flags=re.IGNORECASE):
-        expr = handle_variables(string, variables)
-        return expr
+        return handle_variables(string, variables)
 
     elif re.search(r"(([-+]?)\d+( ?[-+*/]+ ?\d+)+)", string):
         """ this template take an expresion with already replaced variables with their values
         double or more * is not allowed, replace with ^ and add single * - rewrite regex"""
         """Splitting arguments and signs into the list"""
+
+        # can be rewritten with .replace()
         str_as_lst = string.split(" ")
         for arg in range(len(str_as_lst)):
-            if str_as_lst[arg] != " ":
-                if re.match(r"[\-]+$", str_as_lst[arg]):
-                    if len(str_as_lst[arg]) % 2 == 0:
-                        str_as_lst[arg] = "+"
-                    else:
-                        str_as_lst[arg] = "-"
-                elif re.match(r"\+{2,}", str_as_lst[arg]):
+            if re.match(r"[\-]+$", str_as_lst[arg]):
+                if len(str_as_lst[arg]) % 2 == 0:
                     str_as_lst[arg] = "+"
-        return str_as_lst
+                else:
+                    str_as_lst[arg] = "-"
+            elif re.match(r"\+{2,}", str_as_lst[arg]):
+                str_as_lst[arg] = "+"
+        string = " ".join(str_as_lst)
+        return string
     else:
         print("Invalid expression")
 
@@ -57,18 +58,18 @@ def handle_variables(arguments_str, variables):
             print("Unknown variable")
         """ASSIGNMENT SCENARIO"""
     elif re.search(r"=", arguments_str):
-        assign_variable(arguments_str, variables)
+        return assign_variable(arguments_str, variables)
 
-        """ expression with variables """
+
     elif re.match(expr_template, arguments_str):
-        expression_list = compute_var_operations(arguments_str, variables)
+        """ expression with variables """
+        expression_list = compute_var_operations(arguments_str, variables).split(" ")
         if expression_list:
             exp_str = " ".join(expression_list)
-            transformed_expr = transform_into_expression(exp_str, variables)
-            return transformed_expr
+            return transform_into_expression(exp_str, variables)
 
 
-def assign_variable(arguments_str, variables):
+def assign_variable(arguments_str, variables) -> None:
     var_ass_template = re.compile(r"\A\s*[a-z]+\s*=\s*([a-z]+|[\d]+)\s*$", flags=re.IGNORECASE)
     if re.match(var_ass_template, arguments_str):
 
@@ -93,7 +94,7 @@ def assign_variable(arguments_str, variables):
 def compute_var_operations(arguments_str, variables) -> list:
     var_list = re.findall(r"[\w]+", arguments_str, flags=re.IGNORECASE)
     issue_counter = 0
-    arguments_lst = arguments_str.strip(" ").split(" ")
+    arguments_lst = arguments_str.split(" ")
 
     """ test just this function with the complex variable + int expression"""
     for i in range(int(len(arguments_lst))):
@@ -108,13 +109,14 @@ def compute_var_operations(arguments_str, variables) -> list:
     if issue_counter:
         print("Unknown variable")
     else:
-        arguments_lst = transform_into_expression(" ".join(arguments_lst), variables)
-        return arguments_lst
+        return " ".join(arguments_lst)
 
 
-def compute_expression(math_expression):
+def compute_expression(math_expression) -> str:
+    """takes deque object representing math expression and recursively calculates until answer is ready, then returns the answer as a str """
+
     # math_expression = prioritized_computation(math_expression)  # turned of for the stage #6 as no prioritized operations for now
-    if len(math_expression) < 3:
+    if len(math_expression) == 1:
         return "".join(math_expression)
     else:
         arg1 = int(math_expression.popleft())
@@ -136,14 +138,13 @@ def main():
         elif arguments_str.startswith("/"):
             handle_command(arguments_str)
         else:
-            expression_list = transform_into_expression(arguments_str, variables)
-            if expression_list:
-                expression_list = deque(expression_list)
-                result = compute_expression(expression_list)
+            expression = transform_into_expression(arguments_str, variables)
+            if expression:
+                expression_deque = deque(expression.split(" "))
+                result = compute_expression(expression_deque)
                 print(result)
             else:
                 pass
-
 
 if __name__ == '__main__':
     main()
