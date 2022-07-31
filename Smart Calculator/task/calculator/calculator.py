550d6e500ca0617e4cc367bf1f0fd08ae08be521
parseltongist <>
2022-07-31 17:42:47 +0200

    minor structure updates

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -2,31 +2,47 @@ import re
 import operator
 
 from collections import deque
-from validators import MathExpressionValidator
+from validators import MathExpression, MathExpressionFormatter
 from special_commands import SpecialCommand
 
 
-class SmartCalculator(MathExpressionValidator):
-    variables = {}
-
-    def calculate_expression(self):
-        if not self.is_valid_expression():
-            print("Invalid Expression")
-        else:
-            self.expression_formatter()
-            return self.compute_expression()
+class SmartCalculator(MathExpression):
+    variables = dict()
 
     def _is_assignment(self):
         return re.search(r"=", self.expression)
 
-    def assign_variable(self):
+    def compute_expression(self):
+        if self.is_valid_expression():
+
+            expr = MathExpressionFormatter(self.expression)
+            self.expression = expr.get_formatted_expression()
+
+            if self._is_assignment():
+                return self._assign_variable()
+            else:
+                # replacing variables with its values *if any
+                self.expression = self._replace_var_with_value()
+
+            if re.match(r"\A\s*[+-]?\d+$", self.expression):
+                print(self.expression.lstrip("+"))  # strip head plus *if any, but leaving minus
+
+            elif re.search(r"\A[-+]?((\d+\s*[+-/*^)(\s]+\s*)*\d*)*", self.expression):
+                self._postfix_from_infix()  # converting from infix to postfix.
+                result = self._postfix_computation()
+                print(result)
+            else:
+                print("Invalid Expression")
+        else:
+            print("Invalid Expression")
+
+
+    def _assign_variable(self):
         var_ass_template = re.compile(r"\A\s*[a-z]+\s*=\s*([a-z]+|[\d\.]+)\s*$", flags=re.IGNORECASE)
         if re.match(var_ass_template, self.expression):
             var, val = re.split(r"\s*=\s*", self.expression.replace(" ", ""))  # searching in expression without spaces
             # assignment from scratch
-            if val.isnumeric():
-                self.variables[var] = int(val)
-            elif self.is_float(val):
+            if val.isnumeric() or self.is_float(val):
                 self.variables[var] = float(val)
             # reassignment
             else:
@@ -37,22 +53,7 @@ class SmartCalculator(MathExpressionValidator):
         else:
             print("Invalid assignment")
 
-    def compute_expression(self):
-        if self._is_assignment():
-            return self.assign_variable()
-        else:
-            self.expression = self.replace_var_with_value()
-        
-        if re.match(r"\A\s*[+-]?\d+$", self.expression):
-            print(self.expression.lstrip("+"))
-        elif re.search(r"\A[-+]?((\d+\s*[+-/*^)(\s]+\s*)*\d*)*", self.expression):
-            self.postfix_from_infix()
-            result = self.postfix_computation()
-            print(result)
-        else:
-            print("Invalid Expression")
-
-    def postfix_from_infix(self):
+    def _postfix_from_infix(self):
         """ add explanation"""
         priority = {"+": 1, "-": 1, "*": 2, "/": 2, "^": 3}
         self.expression = self.expression.replace("(", "( ").replace(")", " )")
@@ -83,12 +84,11 @@ class SmartCalculator(MathExpressionValidator):
             postfix.append(op_stack.pop())
         self.expression = " ".join(postfix)
 
-    def postfix_computation(self):
+    def _postfix_computation(self):
         ops = {"+": operator.add, "-": operator.sub, "*": operator.mul, "/": operator.truediv, "^": operator.pow}
         calculated_expression = deque()
 
         for x in self.expression.split():
-
             if x.isdigit() or self.is_float(x):
                 calculated_expression.append(x)
 
@@ -101,7 +101,7 @@ class SmartCalculator(MathExpressionValidator):
                     print("Division by zero is forbidden. Please try again.")
                     return main()
 
-        result = calculated_expression[-1]  # the only remaining element in array
+        result = float(calculated_expression[-1])  # the only remaining element in array
 
         if self.is_float_without_decimal_part(result):
             result: float = int(result)  # converting results like (-)x.00 to x
@@ -110,37 +110,18 @@ class SmartCalculator(MathExpressionValidator):
         return result
 
     # replacing vars with their values:
-    def replace_var_with_value(self):
+    def _replace_var_with_value(self):
         var_list = re.findall(r"[a-z]+", self.expression, flags=re.IGNORECASE)
         arguments_lst = self.expression.split()
         for i in range(int(len(arguments_lst))):
-            if arguments_lst[i] in var_list and arguments_lst[i].isalpha():
+            if arguments_lst[i] in var_list:
                 try:
                     arguments_lst[i] = str(self.variables[arguments_lst[i]])
                 except KeyError:
                     print("Unknown variable")
                     return main()
+
         return " ".join(arguments_lst)
-
-    def expression_formatter(self):
-        def convert_duplicate_chars():
-            self.expression = re.sub(r"(\+-)|(-\+)", "-", self.expression)
-            if re.search(r"\d\s*\(", self.expression):  # special case for expressions like a(b+c)...
-                self.expression = re.sub(r"\(", r"* (", self.expression)
-            self.expression = re.sub(r"\+{2,}", "+", self.expression)
-            # replacing all minuse sequences with + or - based on their meaning
-            if re.search(r"-{2,}", self.expression):
-                sign = "+" if len(re.search(r"-{2,}", self.expression).group()) % 2 == 0 else "-"
-                self.expression = re.sub(r"-{2,}", sign, self.expression)
-
-        def split_by_space():
-            ops = {"+", "-", "/", "*", "(", ")", "^"}
-            for x in self.expression:
-                if x in ops:
-                    self.expression = self.expression[0] + self.expression[1::].replace(x, " " + x + " ")  # this allows to fix bugs while inputing "+1 /-1"
-            self.expression = re.sub(r"\s{2,}", " ", self.expression)
-        convert_duplicate_chars()
-        split_by_space()
 
 
 class UserInputProcessor:
@@ -155,7 +136,7 @@ class UserInputProcessor:
             SpecialCommand.execute(user_input)
         else:
             math_expr = SmartCalculator(user_input)
-            math_expr.calculate_expression()
+            math_expr.compute_expression()
 
 
 def main():
--- Smart Calculator/task/calculator/validators.py
+++ Smart Calculator/task/calculator/validators.py
@@ -1,7 +1,7 @@
 import re
 
 
-class MathExpressionValidator:
+class MathExpression:
     def __init__(self, expression):
         self.expression = expression
 
@@ -37,5 +37,32 @@ class MathExpressionValidator:
             return False
 
     @staticmethod
-    def is_float_without_decimal_part(val: (float, int)) -> bool:
-        return val % 1 == 0
+    def is_float_without_decimal_part(val: (str, float, int)) -> bool:
+        return float(val) % 1 == 0
+
+
+class MathExpressionFormatter:
+    def __init__(self, expression):
+        self.expression = expression
+
+    def _convert_duplicate_chars(self):
+        self.expression = re.sub(r"(\+-)|(-\+)", "-", self.expression)
+        if re.search(r"\d\s*\(", self.expression):  # special case for expressions like a(b+c)...
+            self.expression = re.sub(r"\(", r"* (", self.expression)
+        self.expression = re.sub(r"\+{2,}", "+", self.expression)
+        # replacing all minuse sequences with + or - based on their meaning
+        if re.search(r"-{2,}", self.expression):
+            sign = "+" if len(re.search(r"-{2,}", self.expression).group()) % 2 == 0 else "-"
+            self.expression = re.sub(r"-{2,}", sign, self.expression)
+
+    def _split_by_space(self):
+        ops = {"+", "-", "/", "*", "(", ")", "^"}
+        for x in self.expression:
+            if x in ops:
+                self.expression = self.expression[0] + self.expression[1::].replace(x, " " + x + " ")  # this allows to fix bugs while inputing "+1 /-1"
+        self.expression = re.sub(r"\s{2,}", " ", self.expression)
+
+    def get_formatted_expression(self):
+        self._convert_duplicate_chars()
+        self._split_by_space()
+        return self.expression
